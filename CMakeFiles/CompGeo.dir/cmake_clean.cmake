a0acad90e2410d39b3b452bd4b7886321698af6c
Adam Pollack <apollack11@gmail.com>
2016-10-11 17:40:48 -0500

    Set up OpenCV and practiced with test files

---
+++ CMakeFiles/CompGeo.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/CompGeo.dir/test-opencv.cpp.o"
+  "CompGeo.pdb"
+  "CompGeo"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/CompGeo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
